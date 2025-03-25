21ac2ce9188bed266244e9e06e57f3e61f969b53
Тома Гладышева <tommmgll@gmail.com>
2025-03-25 14:22:57 +0300

    add recipes models

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from .models import (Tag, Ingredient, IngredientInRecipe,
+                     Recipe)
 
-# Register your models here.
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    # TODO проверить поля для фильтрации и поиска
+    list_display = ('name', 'slug')
+    search_fields = ('name', 'slug')
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    # TODO проверить поля для фильтрации и поиска
+    list_display = ('name', 'measurement_unit')
+    search_fields = ('name',)
+
+
+class IngredientInRecipeAdmin(admin.TabularInline):
+    model = IngredientInRecipe
+    min_num = 1
+    validate_min = True
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    # TODO перепроверить поля
+    list_display = (
+        'author',
+        'name',
+        'get_favorites_count'
+    )
+
+    search_fields = (
+        'name', 'author__username', 'tags__name'
+    )
+
+    readonly_fields = ('get_favorites_count',)
+    inlines = (IngredientInRecipeAdmin,)
+
+    # TODO добавить фильтрацию по тегам
+
+    def get_favorites_count(self, recipe):
+        return recipe.favorites.count()
+
+    get_favorites_count.short_description = 'Число добавлений в избранное'
+
+
+# TODO проверить нужна ли в админке корзина
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,172 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from users.models import User
 
-# Create your models here.
+# TODO добавить константы
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=128)
+    measurement_unit = models.CharField(max_length=64)
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+        constraints = [
+            models.UniqueConstraint(
+                fields=('name', 'measurement_unit'),
+                name='ingredient_unique'
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.name}, {self.measurement_unit}'
+
+
+class Tag(models.Model):
+    name = models.CharField(
+        max_length=32,
+        unique=True
+    )
+
+    # TODO добавить валидатор для слага ^[-a-zA-Z0-9_]+$
+    slug = models.SlugField(
+        max_length=32,
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+    )
+    name = models.CharField(max_length=50)
+    image = models.ImageField(
+        upload_to='recipes/images/'
+    )
+    text = models.TextField()
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='IngredientInRecipe',
+        verbose_name='Ингредиенты'
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        verbose_name='Тэги'
+    )
+    cooking_time = models.PositiveSmallIntegerField(
+        validators=[
+            MinValueValidator(
+                # TODO вынести в константу и заменить мэсэдж
+                limit_value=1,
+                message='Время приготовления не может быть меньше 1.'
+            )
+        ]
+    )
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+    def __str__(self):
+        return self.name
+
+
+class IngredientInRecipe(models.Model):
+    # TODO дописать verbose
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        related_name='ingredient_in_recipe'
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='ingredient_in_recipe'
+    )
+    amount = models.PositiveIntegerField(
+        validators=[
+            MinValueValidator(
+                # TODO вынести в константу и заменить мэсэдж
+                limit_value=1,
+                message='Количество не может быть меньше 1.'
+            )
+        ]
+    )
+
+    class Meta:
+        verbose_name = 'Ингредиент в рецепте'
+        verbose_name_plural = 'Ингредиенты в рецепте'
+        constraints = [
+            models.UniqueConstraint(
+                fields=('ingredient', 'recipe'),
+                name='ingredient_in_recipe_unique'
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.ingredient} в {self.recipe}'
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='favorites',
+        verbose_name='Пользователь'
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='favorites',
+        verbose_name='Рецепт'
+    )
+
+    class Meta:
+        verbose_name = 'Избранное'
+        verbose_name_plural = 'Избранные'
+        constraints = [
+            models.UniqueConstraint(
+                fields=('user', 'recipe'),
+                name='favorites_unique'
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.user} добавил в избранное {self.recipe}'
+
+
+class ShoppingList(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='shopping_list',
+        verbose_name='Пользователь'
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='shopping_list',
+        verbose_name='Рецепт'
+    )
+
+    class Meta:
+        verbose_name = 'Список покупок'
+        constraints = [
+            models.UniqueConstraint(
+                fields=('user', 'recipe'),
+                name='shopping_list_unique'
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.user} добавил в корзину {self.recipe}'
