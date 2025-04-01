0c06fc7a762d1e2f9d7f289e5545f779f426a303
Тома Гладышева <tommmgll@gmail.com>
2025-04-01 21:31:20 +0100

    add docker production

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -102,7 +102,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = '/backend_static/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
