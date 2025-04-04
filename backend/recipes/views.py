b54026ebd899dc5de1c522a9a526276aff4a76cc
Тома Гладышева <tommmgll@gmail.com>
2025-04-04 10:56:49 +0300

    review fixes 3

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,12 +1,18 @@
 from django.conf import settings
-from django.shortcuts import get_object_or_404, redirect
+from django.core.exceptions import ValidationError
+from django.shortcuts import redirect
+from rest_framework import status
 
 from recipes.models import Recipe
 
 
 def redirect_short_link(request, short_link):
-    short_link = get_object_or_404(Recipe, short_link=short_link)
+    try:
+        recipe = Recipe.objects.get(short_link=short_link)
+    except Recipe.DoesNotExist:
+        raise ValidationError(message='Рецепт ненайден',
+                              code=status.HTTP_404_NOT_FOUND)
 
     return redirect(
-        f'{settings.SITE_URL}/recipes/{short_link.recipe.id}/'
+        f'{settings.SITE_URL}/recipes/{recipe.id}/'
     )
